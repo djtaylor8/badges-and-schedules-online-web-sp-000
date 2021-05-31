389fda8a2e64408171934a01b3b6675dbbdc5e18
DJ Taylor <dtaylor88@gmail.com>
2021-05-31 02:08:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,11 @@ def batch_badge_creator(array)
   end
   new_array
 end
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index(array) {|name, index|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
+  }
+  new_array
+end
