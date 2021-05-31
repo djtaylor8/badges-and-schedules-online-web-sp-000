6e1698a20068ee1f86040b0037f5319caffde00d
DJ Taylor <dtaylor88@gmail.com>
2021-05-31 02:15:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(array)
   new_array = []
-  array.each_with_index(array) do |name, index|
+  array.each_with_index do |name, index|
     new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
   end
   new_array
